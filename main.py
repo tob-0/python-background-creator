f032a8b9e2d266c899c692b13e191fbc0f0637c4
tob <tob@nomail.co>
2022-11-20 20:50:55 +0100

    Refacto

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
-from background_creator import Creator
+from src import Creator
 from os.path import isdir
 from pathlib import Path
 import argparse
 
-BG_TYPES = ['BLURRED', 'BLACK', 'WHITE', 'COLOR', 'BRIGHTEST', 'DARKEST']
+BG_TYPES = ["BLURRED", "BLACK", "WHITE", "COLOR", "BRIGHTEST", "DARKEST"]
+FILE_EXT = ["jpg", "jpeg"]
 
 arg_parser = argparse.ArgumentParser(
-    description='Generate a background for an image with a specific aspect ratio.')
-arg_parser.add_argument('-o', '--output-dir',
-                        default='./output/', type=str, help='Output directory')
-arg_parser.add_argument('-i', '--input', type=str,
-                        help='Input file or folder', required=True)
-arg_parser.add_argument('-a', '--aspect-ratio', type=str,
-                        help='Aspect ratio of the background')
-arg_parser.add_argument('-t', '--type', type=str,
-                        help='Type of background', choices=BG_TYPES, default='BLURRED')
+    description="Generate a background for an image with a specific aspect ratio."
+)
 arg_parser.add_argument(
-    '-s', '--save', action='store_true', default=False, help='Save the output')
+    "-o", "--output-dir", default="./output/", type=str, help="Output directory"
+)
 arg_parser.add_argument(
-    '-v', '--verbose', action='store_true', help='Verbose output')
+    "-i", "--input", type=str, help="Input file or folder", required=True
+)
+arg_parser.add_argument(
+    "-a", "--aspect-ratio", type=str, help="Aspect ratio of the background"
+)
+arg_parser.add_argument(
+    "-t",
+    "--type",
+    type=str,
+    help="Type of background",
+    choices=BG_TYPES,
+    default="BLURRED",
+)
+arg_parser.add_argument(
+    "-s", "--save", action="store_true", default=False, help="Save the output"
+)
+arg_parser.add_argument("-v", "--verbose", action="store_true", help="Verbose output")
 
 
 def main(args):
@@ -29,23 +40,24 @@ def main(args):
         content = input_path.iterdir()
         for obj in content:
             if obj.is_file():
-                file_extension = obj.name.split('.')[-1]
-                if file_extension in ['jpg']:
+                file_extension = obj.name.split(".")[-1]
+                if file_extension in FILE_EXT:
                     files += [str(obj.absolute())]
 
     else:
         if input_path.is_file():
-            file_extension = input_path.name.split('.')[-1]
-            if file_extension in ['jpg']:
+            file_extension = input_path.name.split(".")[-1]
+            if file_extension in FILE_EXT:
                 files += [input_path.absolute()]
         files += [args.input]
-    aspect_ratio = args.aspect_ratio or '4/5'
+    aspect_ratio = args.aspect_ratio or "4/5"
     creator = Creator(verbose=args.verbose, aspect_ratio=aspect_ratio)
 
     for file in files:
-        creator.generate(file, background_type=args.type,
-                         save=args.save, save_folder=args.output_dir)
+        creator.generate(
+            file, background_type=args.type, save=args.save, save_folder=args.output_dir
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(arg_parser.parse_args())
--- src/backgrounder_tob/creator.py
+++ src/backgrounder_tob/creator.py
@@ -1,4 +1,3 @@
-
 from uuid import uuid4
 from PIL import Image, ImageFilter
 import re
@@ -7,7 +6,14 @@ from pathlib import Path
 
 
 class Creator:
-    def __init__(self, aspect_ratio: str = "4/5", orientation: str = "vertical", img: Optional[str] = None, output_dir: str = './', verbose: bool = False) -> NoReturn:
+    def __init__(
+        self,
+        aspect_ratio: str = "4/5",
+        orientation: str = "vertical",
+        img: Optional[str] = None,
+        output_dir: str = "./",
+        verbose: bool = False,
+    ) -> NoReturn:
         """Create an object for background creation with a specific aspect ratio.
 
         Args:
@@ -32,7 +38,8 @@ class Creator:
             result = re.search(r"(\d+)[:/-|]{1}(\d+)", aspect_ratio_str)
             if result:
                 aspect_ratio_dimensions = list(
-                    map(lambda dim: int(dim), result.groups()))
+                    map(lambda dim: int(dim), result.groups())
+                )
                 if len(aspect_ratio_dimensions) == 2:
                     if aspect_ratio_dimensions[0] > aspect_ratio_dimensions[1]:
                         self.orientation = "horizontal"
@@ -40,11 +47,9 @@ class Creator:
                         self.orientation = "vertical"
 
                     if self.orientation == "vertical":
-                        ratio = aspect_ratio_dimensions[0] / \
-                            aspect_ratio_dimensions[1]
+                        ratio = aspect_ratio_dimensions[0] / aspect_ratio_dimensions[1]
                     elif self.orientation == "horizontal":
-                        ratio = aspect_ratio_dimensions[1] / \
-                            aspect_ratio_dimensions[0]
+                        ratio = aspect_ratio_dimensions[1] / aspect_ratio_dimensions[0]
 
         return ratio
 
@@ -62,7 +67,9 @@ class Creator:
         bg.paste(img, (offset, 0))
         return bg
 
-    def _clrd_bg(self, img: Image = None, color: Optional[tuple[int]] = (0, 0, 0)) -> Image:
+    def _clrd_bg(
+        self, img: Image = None, color: Optional[tuple[int]] = (0, 0, 0)
+    ) -> Image:
         img = img or self.img
         src_w, src_h = img.size
         bg_w = int(src_h * self.ratio)
@@ -71,17 +78,21 @@ class Creator:
         bg.paste(img, (offset, 0))
         return bg
 
-    def _brtst_c_bg(self, img: Image = None) -> Image:
+    def _brtst_c_bg(
+        self, img: Image = None, threshold: int = 150, threshold_count: int = 3
+    ) -> Image:
         img = img or self.img
         rgb_img = img.convert("RGB")
         src_w, src_h = img.size
         bg_w = int(src_h * self.ratio)
         brightest_color = (0, 0, 0)
         highest_brightness = 0
-        for w in [0, src_w-1]:
-            for h in range(src_h//2):
+        for w in [0, src_w - 1]:
+            for h in range(src_h // 2):
                 r, g, b = rgb_img.getpixel((w, h))
-                perceived_brightness = 0.2126*r + 0.7152*g + 0.0722*b
+                if len([c for c in (r, g, b) if c > threshold]) >= threshold_count:
+                    continue
+                perceived_brightness = 0.2126 * r + 0.7152 * g + 0.0722 * b
                 if perceived_brightness >= highest_brightness:
                     highest_brightness = perceived_brightness
                     brightest_color = (r, g, b)
@@ -98,10 +109,10 @@ class Creator:
         bg_w = int(src_h * self.ratio)
         darkest_color = (255, 255, 255)
         highest_brightness = 100
-        for w in [0, src_w-1]:
+        for w in [0, src_w - 1]:
             for h in range(src_h):
                 r, g, b = rgb_img.getpixel((w, h))
-                perceived_brightness = 0.2126*r + 0.7152*g + 0.0722*b
+                perceived_brightness = 0.2126 * r + 0.7152 * g + 0.0722 * b
                 if perceived_brightness <= highest_brightness:
                     highest_brightness = perceived_brightness
                     brightest_color = (r, g, b)
@@ -111,30 +122,38 @@ class Creator:
         bg.paste(img, (offset, 0))
         return bg
 
-    def generate(self, img=None, background_type: Optional[str] = "BLURRED", save: Optional[bool] = False, save_folder: Optional[str] = None, show: Optional[bool] = False, *args, **kwargs):
+    def generate(
+        self,
+        img=None,
+        background_type: Optional[str] = "BLURRED",
+        save: Optional[bool] = False,
+        save_folder: Optional[str] = None,
+        show: Optional[bool] = False,
+        *args,
+        **kwargs
+    ):
 
         try:
             img = img or self.img
             save_folder = save_folder or self.output_dir
-            if save_folder[-1] != '/':
-                save_folder += '/'
+            if save_folder[-1] != "/":
+                save_folder += "/"
             save_path = None
 
             if type(img) == str:
                 img = Image.open(img)
             elif type(img) == Image:
-                raise ValueError(
-                    '"img" should be either a str or PIL.Image')
+                raise ValueError('"img" should be either a str or PIL.Image')
             if save:
-                save_path = save_folder + \
-                    str(uuid4()) + \
-                    '.' + img.filename.split('.')[-1]
+                save_path = (
+                    save_folder + str(uuid4()) + "." + img.filename.split(".")[-1]
+                )
                 Path(save_folder).mkdir(exist_ok=True, parents=True)
 
             if background_type == "WHITE":
-                kwargs['color'] = (255, 255, 255)
+                kwargs["color"] = (255, 255, 255)
             elif background_type == "BLACK":
-                kwargs['color'] = (0, 0, 0)
+                kwargs["color"] = (0, 0, 0)
 
             backgrounded_image = {
                 "BLURRED": self._blrd_bg,
@@ -142,7 +161,7 @@ class Creator:
                 "WHITE": self._clrd_bg,
                 "BRIGHTEST": self._brtst_c_bg,
                 "DARKEST": self._drkst_c_bg,
-                "COLOR": self._clrd_bg
+                "COLOR": self._clrd_bg,
             }[background_type](img, *args, **kwargs)
             if save:
                 backgrounded_image.save(save_path, quality=95)
